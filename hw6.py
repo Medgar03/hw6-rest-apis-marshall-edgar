a34edc633ad07e27f9f92ef67ee41e7d94758d06
Medgar03 <marshalledgar3@gmail.com>
2023-02-28 16:01:06 -0600

    Update hw6.py
    fixed api issues

--- hw6.py
+++ hw6.py
@@ -7,16 +7,18 @@ load_dotenv(find_dotenv())
 
 def get_top_10_weekly_trending_movies():
     TMDB_BASE_URL = "https://api.themoviedb.org/3"
-    TMDB_TRENDING_MOVIES_PATH = "/trending/movies/week"
+    TMDB_TRENDING_MOVIES_PATH = "/trending/movie/week"
+
 
     trending_movies = requests.get(
         TMDB_BASE_URL + TMDB_TRENDING_MOVIES_PATH,
         params ={
-            "api-key": os.get("TMDB_API_KEY"),
+            "api_key": os.getenv("TMDB_API_KEY"),
         }
     )
     
-    trending_movies_list =  trending_movies.json()["results"]
+    trending_movies_list = trending_movies.json()["results"]
+    
     
     for i in range(10):
         print(trending_movies_list[i]["title"])
